33868017b023698434a92a4b892e3866be8a5e4f
Uchino Jun <uchino@sansan.com>
2018-03-05 11:45:28 +0900

    enable ps

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ import click
 @click.option('-u', '--username', 'username', required=True)
 @click.option('-p', '--password', 'password', required=True)
 @click.option('-c', '--command', 'command', required=True)
+
+
 def execute_winrm(hostname, username, password, command):
     s = winrm.Session(hostname, auth=(username, password))
-    r = s.run_cmd(command)
+    r = s.run_ps(command)
     print(re.sub(rb'\r\n', b'\n', r.std_out).decode('utf-8'))
 
 if __name__ == '__main__':
     execute_winrm()
-
